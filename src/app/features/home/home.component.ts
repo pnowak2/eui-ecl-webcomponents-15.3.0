ff880244e94246ffb95eb46a791400b1d473c939
Piotr Nowak <p.nowak2@gmail.com>
2023-03-22 12:06:46 +0100

    done

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,11 +1,30 @@
 import { Component, Inject } from '@angular/core';
 import { CONFIG_TOKEN, EuiAppConfig } from '@eui/core';
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
     templateUrl: './home.component.html',
 })
 export class HomeComponent {
+    accordionItems = [
+        { id: 'id-' + uuidv4(), label: 'Accordion item 1', content: 'Content 1' },
+        { id: 'id-' + uuidv4(), label: 'Accordion item 2', content: 'Content 2' },
+        { id: 'id-' + uuidv4(), label: 'Accordion item 3', content: 'Content 3' },
+    ];
+
     constructor(@Inject(CONFIG_TOKEN) private config: EuiAppConfig) {
         console.log(config);
+    }
+
+    onAddSection(sectionName: HTMLEclInputElement) {
+        const input: HTMLInputElement = sectionName.querySelector('.ecl-text-input');
+
+        const id = 'id-' + uuidv4();
+        this.accordionItems = [
+            ...this.accordionItems,
+            { id, label: input.value, content: id }
+        ];
+
+        input.value = '';
     }
 }
